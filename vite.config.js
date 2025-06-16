0b39cb3b5fbb5b70c8ab1fff289fdb675d8a5e07
Jefferson <hbrcomercialssa@gmail.com>
2025-06-15 22:03:14 -0300

    index

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,16 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/pontosimulado/",
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "react-router-dom"],
+          supabase: ["@supabase/supabase-js"],
+        },
+      },
+    },
+  },
 });
